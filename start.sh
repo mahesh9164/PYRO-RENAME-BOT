759cd94211759f2023accf603643be97d96d1d98
amit4405 <83196950+amit4405@users.noreply.github.com>
2022-08-22 07:42:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@ apt install git -y
 pip3 install -U pip
 
 git clone https://github.com/amit4406/PYRO-RENAME-BOT.git          
-cd Github_Repo
 pip3 install -U -r requirements.txt
 
 echo "𝙱𝚘𝚝 𝙸𝚜 𝚂𝚝𝚊𝚛𝚝𝚒𝚗𝚐........"
